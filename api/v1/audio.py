3b1121fab51924ad7296d73843ea36aef15530a9
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-06-14 12:42:59 +0300

    feat: router for uploading audio files

--- api/v1/audio.py
+++ api/v1/audio.py
@@ -1,3 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
+from uuid import uuid4, UUID
+from .models import UploadFileResponse
+import aiofiles
 
 router = APIRouter(prefix="/audio", tags=["audio"])
+
+
+@router.post("/upload", response_model=UploadFile)
+async def upload_audio(upload_file: UploadFile):
+    file_id = uuid4()
+    async with aiofiles.open("/temp_data/" + str(file_id), "wb") as file:
+        byte_content = await upload_file.read()
+        await file.write(byte_content)
+    return UploadFileResponse(file_id=file_id)
