165975a3315ea2d3757c2a605a964b0b1b7f970a
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-06-14 13:09:46 +0300

    feat: document kind validation

--- api/v1/audio.py
+++ api/v1/audio.py
@@ -10,6 +10,10 @@ router = APIRouter(prefix="/audio", tags=["audio"])
 async def upload_audio(upload_file: UploadFile) -> UploadFileResponse:
     file_id = uuid4()
 
+    # Here we using MIME types specification, which have format
+    # "kind/name". In the following code, we checking that the kind of document
+    # is "audio". It is the easiest methods to allow uploading any audio format files.
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
     if (
         upload_file.content_type is None
         or upload_file.content_type.split("/")[0] != "audio"
