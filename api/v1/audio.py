f579ece0f4c5f65f39ed477215ca446694967611
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-06-14 13:02:46 +0300

    feat: add validation for uploading audio files

--- api/v1/audio.py
+++ api/v1/audio.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, HTTPException, status
 from uuid import uuid4, UUID
 from .models import UploadFileResponse
 import aiofiles
@@ -9,7 +9,18 @@ router = APIRouter(prefix="/audio", tags=["audio"])
 @router.post("/upload", response_model=UploadFileResponse)
 async def upload_audio(upload_file: UploadFile) -> UploadFileResponse:
     file_id = uuid4()
+
+    if (
+        upload_file.content_type is None
+        or upload_file.content_type.split("/")[0] != "audio"
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Only audio files uploads are allowed",
+        )
+
     async with aiofiles.open("./temp_data/audio/" + str(file_id), "wb") as file:
         byte_content = await upload_file.read()
         await file.write(byte_content)
+
     return UploadFileResponse(file_id=file_id)
