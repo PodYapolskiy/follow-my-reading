10faaa0dbac4ad6b87746cbc7ba44b54a290a63e
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-06-14 12:44:21 +0300

    feat: change path temp_data -> temp_data/audio

--- api/v1/audio.py
+++ api/v1/audio.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/audio", tags=["audio"])
 @router.post("/upload", response_model=UploadFile)
 async def upload_audio(upload_file: UploadFile):
     file_id = uuid4()
-    async with aiofiles.open("/temp_data/" + str(file_id), "wb") as file:
+    async with aiofiles.open("/temp_data/audio/" + str(file_id), "wb") as file:
         byte_content = await upload_file.read()
         await file.write(byte_content)
     return UploadFileResponse(file_id=file_id)
