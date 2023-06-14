fc44459af97add88f9cada5c0dc7bd59e6ac1914
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-06-14 12:45:29 +0300

    feat: router for uploading images

--- api/v1/image.py
+++ api/v1/image.py
@@ -1,3 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
+from uuid import uuid4, UUID
+from .models import UploadFileResponse
+import aiofiles
+
 
 router = APIRouter(prefix="/image", tags=["image"])
+
+
+@router.post("/upload", response_model=UploadFile)
+async def upload_image(upload_file: UploadFile):
+    file_id = uuid4()
+    async with aiofiles.open("/temp_data/image/" + str(file_id), "wb") as file:
+        byte_content = await upload_file.read()
+        await file.write(byte_content)
+    return UploadFileResponse(file_id=file_id)
