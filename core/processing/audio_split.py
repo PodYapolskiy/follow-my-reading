4c145a9cb29f40294f14c0b1e6c903d658cff270
f.smirnov <f.smirnov@innopolis.university>
2023-06-21 14:07:52 +0300

    feat: added function for converting between dbfs and percents

--- core/processing/audio_split.py
+++ core/processing/audio_split.py
@@ -1,8 +1,17 @@
-from pydub import AudioSegment
+from pydub import AudioSegment, silence
 from os import path
+from math import log10 as lg
 
 
-def split(file, intervals):
+def dbfs_to_percents(dbfs):
+    return 10 ** (dbfs / 20)
+
+
+def percents_to_dbfs(percents):
+    return 20 * lg(percents)
+
+
+def split_audio(file, intervals):
     # file is the path to the file to be split
     # intervals is the set of intervals to be returned in format of [(begin(seconds), length(seconds)),]
     # returns the absolute path to where the split segments were stored
@@ -15,5 +24,4 @@ def split(file, intervals):
               int((intervals[i][0] + intervals[i][1])*1000)].export(f"{store_path}/{i}.mp3", format="mp3")
 
     return path.abspath(store_path)
-
 
