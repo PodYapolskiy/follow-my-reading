2cbe080a03a0327ebbf992648670a738736a3f66
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-06-28 23:40:14 +0300

    fix: fix bugs, add deps, include router

--- api/v1/auth.py
+++ api/v1/auth.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from functools import lru_cache
-from typing import Annotated
+from typing import Annotated, Dict
 from .models import RegisterResponse
 from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -11,8 +11,6 @@ from .config import Settings
 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-users_db = {}
 
 
 @lru_cache
@@ -40,11 +38,13 @@ class UserInDB(User):
     hashed_password: str
 
 
+users_db: Dict[str, UserInDB] = {}  # todo: replace with database connection
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/auth/token")
 
-auth_router = APIRouter(prefix="/auth", tags=['auth'])
+router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 def verify_password(plain_password, hashed_password):
@@ -58,7 +58,7 @@ def get_password_hash(password):
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
-        return UserInDB(**user_dict)
+        return UserInDB.parse_obj(user_dict)
     return False
 
 
@@ -71,19 +71,24 @@ def authenticate_user(db, username: str, password: str):
     return user
 
 
-def create_access_token(data: dict, setting: Annotated[Settings, Depends(get_settings)],  expires_delta: timedelta | None = None):
+def create_access_token(
+    data: dict,
+    expires_delta: timedelta | None = None,
+):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, setting.SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, get_settings().SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
-                           setting: Annotated[Settings, Depends(get_settings)]):
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    setting: Annotated[Settings, Depends(get_settings)],
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -97,23 +102,28 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
+
+    if token_data.username is None:
+        raise credentials_exception
+
     user = get_user(users_db, username=token_data.username)
     if user is None:
         raise credentials_exception
+
     return user
 
 
 async def get_current_active_user(
-        current_user: Annotated[User, Depends(get_current_user)]
+    current_user: Annotated[User, Depends(get_current_user)]
 ):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
-@auth_router.post("/token", response_model=Token)
+@router.post("/token", response_model=Token)
 async def login_for_access_token(
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ):
     user = authenticate_user(users_db, form_data.username, form_data.password)
     if not user:
@@ -124,29 +134,36 @@ async def login_for_access_token(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user.username},
+        expires_delta=access_token_expires,
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@auth_router.get("/users/me/", response_model=User)
+@router.get("/users/me/", response_model=User)
 async def read_users_me(
-        current_user: Annotated[User, Depends(get_current_active_user)]
+    current_user: Annotated[User, Depends(get_current_active_user)]
 ):
     return current_user
 
 
-@auth_router.put("/register", response_model=RegisterResponse)
-async def register_user(username: str, password: str, email: str | None = None, full_name: str | None = None):
-    # I don't know whether do we need to set password requirements or not, and if I checked everything or not
-    # (most probably not, but nothing else comes to my mind, or I have schizophrenia, which is also possible)
-    # And did I even implemented it right?
+@router.put("/register", response_model=RegisterResponse)
+async def register_user(
+    username: str, password: str, email: str | None = None, full_name: str | None = None
+):
     hashed_password = get_password_hash(password)
     if get_user(users_db, username):
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="username is already taken")
-    users_db.update({username: UserInDB(username=username,
-                                        email=email,
-                                        full_name=full_name,
-                                        disabled=False,
-                                        hashed_password=hashed_password)})
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="username is already taken",
+        )
+
+    users_db[username] = UserInDB(
+        username=username,
+        email=email,
+        full_name=full_name,
+        disabled=False,
+        hashed_password=hashed_password,
+    )
+
     return RegisterResponse(text="Registered successfully.")
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from api.v1 import audio_router as audio_router_v1
 from api.v1 import image_router as image_router_v1
 from api.v1 import task_router as task_router_v1
+from api.v1 import auth_router as auth_router_v1
 
 nest_asyncio.apply()
 
@@ -13,3 +14,4 @@ app = FastAPI()
 app.include_router(audio_router_v1, prefix="/v1")
 app.include_router(image_router_v1, prefix="/v1")
 app.include_router(task_router_v1, prefix="/v1")
+app.include_router(auth_router_v1, prefix="/v1")
