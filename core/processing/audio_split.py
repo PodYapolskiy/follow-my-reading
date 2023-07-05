01a6bc94cf8c89b3ea69e5930f91e3a0dfd05ed9
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-07-05 14:15:08 +0300

    style: type aliases for audio split

--- core/processing/audio_split.py
+++ core/processing/audio_split.py
@@ -2,41 +2,44 @@ from math import log10 as lg
 from os import mkdir, path
 
 from librosa import get_duration
-from uuid import uuid4
+from uuid import uuid4, UUID
 from pydub import AudioSegment, silence
+from typing import List, Tuple
 
 
-def duration(audio: str):
+def duration(audio: str) -> float:
     filepath = "./temp_data/audio/" + audio
     return get_duration(path=filepath)
 
 
-def dbfs_to_percents(dbfs):
+def dbfs_to_percents(dbfs: float) -> float:
     return 10 ** (dbfs / 20)
 
 
-def percents_to_dbfs(percents):
+def percents_to_dbfs(percents: float) -> float:
     return 20 * lg(percents)
 
 
-def split_audio(file: str | AudioSegment, intervals):
+def split_audio(  # type: ignore
+    file: str | AudioSegment, intervals: List[Tuple[float, float]]
+) -> List[UUID]:
     # file is the path to the file to be split or an object
     # intervals is the set of intervals to be returned in format of [(begin(seconds), end(seconds)),]
     # returns the absolute path to where the split segments were stored
 
-    store_path = "./temp_data/audio"  # a temporal change just for extract_text to work with split audio
+    store_path = "./temp_data/audio"
 
     if not path.exists(store_path):
         mkdir(store_path)
 
-    if type(file) == str:
-        audio = AudioSegment.from_file(file)
-    elif type(file) == AudioSegment:
+    if isinstance(file, str):
+        audio: AudioSegment = AudioSegment.from_file(file)  # type: ignore
+    elif isinstance(file, AudioSegment):
         audio = file
     else:
         raise TypeError("Invalid argument")
 
-    files = []
+    files: List[UUID] = []
     for i in range(len(intervals)):
         file_id = uuid4()
         audio[int(intervals[i][0] * 1000) : int(intervals[i][1] * 1000)].export(
@@ -47,12 +50,14 @@ def split_audio(file: str | AudioSegment, intervals):
     return files
 
 
-def split_silence(file, max_interval=30, cutoff_ratio=0.05):
+def split_silence(
+    file: str, max_interval: float = 30, cutoff_ratio: float = 0.05
+) -> Tuple[List[UUID], List[Tuple[int, int]]]:
     # file is the file to be split
     # max_interval is the maximum length of the split audio
     # cutoff ratio is the noise level that is accepted for silence as a ratio of the max noise level
 
-    audio = AudioSegment.from_file(file, file[file.rfind(".") + 1 :])
+    audio: AudioSegment = AudioSegment.from_file(file, file[file.rfind(".") + 1 :])  # type: ignore
     max_dbfs = audio.max_dBFS
     noise_level = percents_to_dbfs(cutoff_ratio * dbfs_to_percents(max_dbfs))
 
