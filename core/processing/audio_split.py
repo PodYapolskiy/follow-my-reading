3fb2a02fbceff5b461ffac60d4f6483dd95f4519
f.smirnov <f.smirnov@innopolis.university>
2023-07-06 19:57:23 +0300

    fix: added documentation to the algorithms in processing

--- core/processing/audio_split.py
+++ core/processing/audio_split.py
@@ -1,8 +1,9 @@
 from math import log10 as lg
 from os import mkdir, path
+from typing import List, Tuple
 
 from librosa import get_duration
-from uuid import uuid4
+from uuid import uuid4, UUID
 from pydub import AudioSegment, silence
 
 
@@ -11,24 +12,40 @@ def duration(audio: str):
     return get_duration(path=filepath)
 
 
-def dbfs_to_percents(dbfs):
+def dbfs_to_fraction(dbfs: float) -> float:
+    """
+    Converts dbfs to fraction of max volume
+    :param dbfs: dbfs to be converted
+    :return: the fraction (float)
+    """
     return 10 ** (dbfs / 20)
 
 
-def percents_to_dbfs(percents):
-    return 20 * lg(percents)
+def fraction_to_dbfs(fraction: float) -> float:
+    """
+    Converts percents of max volume into dbfs
+    :param fraction: the fraction of max volume
+    :return: the dbfs (float)
+    """
+    return 20 * lg(fraction)
 
 
-def split_audio(file: str | AudioSegment, intervals):
-    # file is the path to the file to be split or an object
-    # intervals is the set of intervals to be returned in format of [(begin(seconds), end(seconds)),]
-    # returns the absolute path to where the split segments were stored
+def split_audio(file: str | AudioSegment, intervals: List[Tuple[float, float]]) -> List[UUID]:
+    """
+    Splits the audio using timestamps for beginning and end
+    Supports mul
+    :param file: path to the audio file or pydub.AudioSegment
+    :param intervals: a list of segments, given by the timestamps to the beginning and end (in seconds)
+    :return: the uuids of the cut-up files (in order of appearance in intervals)
+    """
 
     store_path = "./temp_data/audio"  # a temporal change just for extract_text to work with split audio
 
+    # Creating the directory to store the files if it does not exist
     if not path.exists(store_path):
         mkdir(store_path)
 
+    # Creating the pydub.AudioSegment if it is not already created
     if type(file) == str:
         audio = AudioSegment.from_file(file)
     elif type(file) == AudioSegment:
@@ -36,10 +53,11 @@ def split_audio(file: str | AudioSegment, intervals):
     else:
         raise TypeError("Invalid argument")
 
-    files = []
+    # Cutting up the file and storing it
+    files: List[UUID] = []
     for i in range(len(intervals)):
         file_id = uuid4()
-        audio[int(intervals[i][0] * 1000) : int(intervals[i][1] * 1000)].export(
+        audio[int(intervals[i][0] * 1000): int(intervals[i][1] * 1000)].export(
             f"{store_path}/{file_id}", format="mp3"
         )
         files.append(file_id)
@@ -47,20 +65,29 @@ def split_audio(file: str | AudioSegment, intervals):
     return files
 
 
-def split_silence(file, max_interval=30, cutoff_ratio=0.05):
-    # file is the file to be split
-    # max_interval is the maximum length of the split audio
-    # cutoff ratio is the noise level that is accepted for silence as a ratio of the max noise level
+def split_silence(file: str, max_interval=30, cutoff_ratio=0.05) -> Tuple[List[UUID], List[Tuple[int, int]]]:
+    """
+    Splits the audio file into segments of some length
+    Only cuts on silence, never cuts words
+    Leaves a 50 ms buffer around every segment
+    :param file: the path to the file to be split
+    :param max_interval: the maximum length of a segment
+    :param cutoff_ratio: the percentage of max volume at which a segment is considered "silent"
+    :return: the list of the UUIDs of all the cut-up segments and the intervals at which they were cut
+    """
 
+    # Creating the pydub.AudioSegment and preparing some variables
     audio = AudioSegment.from_file(file, file[file.rfind(".") + 1 :])
     max_dbfs = audio.max_dBFS
-    noise_level = percents_to_dbfs(cutoff_ratio * dbfs_to_percents(max_dbfs))
+    noise_level = fraction_to_dbfs(cutoff_ratio * dbfs_to_fraction(max_dbfs))
 
+    # Detecting the silence
     silence_chunks = silence.detect_silence(
         audio, min_silence_len=100, silence_thresh=noise_level
     )
     audio_intervals = []
 
+    # Padding the audio if it does not begin or end with silence
     max_interval *= 1000
     if silence_chunks[0][0] != 0:
         audio = AudioSegment.silent(100) + audio
@@ -72,6 +99,7 @@ def split_silence(file, max_interval=30, cutoff_ratio=0.05):
         audio = audio + AudioSegment.silent(100)
     new_beg = silence_chunks[0][1] - 50
 
+    # Accumulating words until we reach the threshold
     for i in range(1, len(silence_chunks)):
         if 50 + silence_chunks[i][0] - new_beg > max_interval:
             audio_intervals.append((new_beg, silence_chunks[i - 1][0] + 50))
@@ -79,7 +107,8 @@ def split_silence(file, max_interval=30, cutoff_ratio=0.05):
 
     audio_intervals.append((new_beg, silence_chunks[-1][0] + 50))
 
+    # Split by counted intervals and return the result
     return (
         split_audio(audio, [(i[0] / 1000, i[1] / 1000) for i in audio_intervals]),
-        audio_intervals,
+        audio_intervals
     )
