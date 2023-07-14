12f6413d187f38016390876e896783e3381589fa
Anton Kudryavtsev <a.kudryavtsev@innopolis.university>
2023-07-14 16:44:01 +0300

    style: sort auth endpoints for better swagger interface

--- api/v1/auth.py
+++ api/v1/auth.py
@@ -24,6 +24,58 @@ config = get_config()
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
+@router.put(
+    "/register",
+    response_model=RegisterResponse,
+    status_code=200,
+    summary="""The endpoint `/register` registers a new user by storing their username, password, email, and
+    full name in a Redis database.""",
+    responses={
+        422: {
+            "description": "The specified username is already taken",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Username is already taken",
+                    }
+                }
+            },
+        },
+    },
+)
+async def register_user(
+    username: str,
+    password: str,
+    conn: Annotated[aioredis.Redis, Depends(get_redis_connection)],
+    email: str | None = None,
+    full_name: str | None = None,
+) -> RegisterResponse:
+    """
+    Parameters:
+    - **username**: The "username: parameter is a string representing the username of the user being registered
+    - **password**: The "password" parameter is a string that represents the user's password
+    - **email**: The "email" parameter is an optional string that represents the email address of the user
+    - **full_name**: The "full_name" parameter is an optional parameter that represents the full name of the user
+    """
+    hashed_password = get_password_hash(password)
+    if await conn.hexists("users", username):
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Username is already taken",
+        )
+    encoded_data = UserInDB.json(
+        UserInDB(
+            username=username,
+            email=email,
+            full_name=full_name,
+            disabled=False,
+            hashed_password=hashed_password,
+        )
+    )
+    await conn.hset("users", username, encoded_data)
+    return RegisterResponse(text="Registered successfully.")
+
+
 @router.post(
     "/token",
     response_model=Token,
@@ -102,55 +154,3 @@ async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ) -> User:
     return current_user
-
-
-@router.put(
-    "/register",
-    response_model=RegisterResponse,
-    status_code=200,
-    summary="""The endpoint `/register` registers a new user by storing their username, password, email, and
-    full name in a Redis database.""",
-    responses={
-        422: {
-            "description": "The specified username is already taken",
-            "content": {
-                "application/json": {
-                    "example": {
-                        "detail": "Username is already taken",
-                    }
-                }
-            },
-        },
-    },
-)
-async def register_user(
-    username: str,
-    password: str,
-    conn: Annotated[aioredis.Redis, Depends(get_redis_connection)],
-    email: str | None = None,
-    full_name: str | None = None,
-) -> RegisterResponse:
-    """
-    Parameters:
-    - **username**: The "username: parameter is a string representing the username of the user being registered
-    - **password**: The "password" parameter is a string that represents the user's password
-    - **email**: The "email" parameter is an optional string that represents the email address of the user
-    - **full_name**: The "full_name" parameter is an optional parameter that represents the full name of the user
-    """
-    hashed_password = get_password_hash(password)
-    if await conn.hexists("users", username):
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail="Username is already taken",
-        )
-    encoded_data = UserInDB.json(
-        UserInDB(
-            username=username,
-            email=email,
-            full_name=full_name,
-            disabled=False,
-            hashed_password=hashed_password,
-        )
-    )
-    await conn.hset("users", username, encoded_data)
-    return RegisterResponse(text="Registered successfully.")
